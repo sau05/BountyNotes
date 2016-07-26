f644e1b0a6c68822eb75cb721f7c9df15b36cfdc
saurabh <kirisaurabh@gmail.com>
2016-07-26 23:29:39 +0530

    cross animation, and delete note added

--- app/src/main/java/com/artoo/bountynotes/MainActivity.java
+++ app/src/main/java/com/artoo/bountynotes/MainActivity.java
@@ -187,6 +187,9 @@ public class MainActivity extends AppCompatActivity implements RecyclerItemClick
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==android.R.id.home){
+            onBackPressed();
+        }
         return super.onOptionsItemSelected(item);
     }
 
